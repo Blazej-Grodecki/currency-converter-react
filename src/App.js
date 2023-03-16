1f899be25221211cc3a921f73f77048adedb9938
Blazej Grodecki <b.grodecki@gmail.com>
2023-03-16 22:50:51 +0100

    added Form component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,13 @@
 import Header from "./Header";
+import Form from "./Form";
 
 function App() {
   return (
     <div className="container">
       <Header title="Kalkulator azjatyckich walut" />
-      <form className="form js-form">
-        <fieldset className="form__fieldset">
-          <legend className="form__legend">Przelicz walutę</legend>
-          <p>
-            <label>
-              <span className="form__labelText">Wybierz walutę</span>
-              <select
-                className="form__field js-currency"
-                name="currency"
-                title="Wybierz walutę"
-              >
-                <option value="CNY">CNY (yuan)</option>
-                <option value="PHP">PHP (peso)</option>
-                <option value="INR">INR (rupia)</option>
-                <option value="JPY">JPY (jen)</option>
-                <option value="KRW">KRW (won)</option>
-                <option value="SGD">SGD (dolar)</option>
-                <option value="MYR">MYR (ringgit)</option>
-                <option value="THB">THB (baht)</option>
-              </select>
-            </label>
-          </p>
-          <p>
-            <label>
-              <span className="form__labelText">Wpisz kwotę w PLN: </span>
-              <input
-                className="form__field js-amount"
-                type="number"
-                min="0.01"
-                step="0.01"
-                required
-                title="Wpisz kwotę"
-              />
-            </label>
-          </p>
-        </fieldset>
-        <p>
-          <button className="form__button">Przelicz</button>
-        </p>
-      </form>
+      <Form />
       <p className="form__result">
-        <span className="js-result"></span>
+        <span></span>
       </p>
       <footer className="footer">
         <p>
