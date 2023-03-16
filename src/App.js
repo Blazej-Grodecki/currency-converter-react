16f9aed64454cbc31bfaa02ca2a92f05583a6d8e
Blazej Grodecki <b.grodecki@gmail.com>
2023-03-16 22:55:26 +0100

    added Footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Form from "./Form";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -9,12 +10,7 @@ function App() {
       <p className="form__result">
         <span></span>
       </p>
-      <footer className="footer">
-        <p>
-          CNY = 0.6610, PHP = 0.0818, INR = 5.8179 / 100, JPY = 3.2461 / 100
-        </p>
-        <p>KRW = 0.3382 / 100, SGD = 3.3886, MYR = 1.0104, THB = 0.1277</p>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <p>CNY = 0.6610, PHP = 0.0818, INR = 5.8179 / 100, JPY = 3.2461 / 100</p>
+    <p>KRW = 0.3382 / 100, SGD = 3.3886, MYR = 1.0104, THB = 0.1277</p>
+  </footer>
+);
+
+export default Footer;
