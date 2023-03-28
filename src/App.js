b7d64996c14239e91281a83c70728328d890cdd4
Blazej Grodecki <b.grodecki@gmail.com>
2023-03-28 11:36:40 +0200

    added Rates component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./Header";
 import Form from "./Form";
 import Footer from "./Footer";
 import Container from "./Container";
+import Rates from "./Rates";
 
 function App() {
   return (
---
+++ src/Rates/index.js
@@ -0,0 +1,12 @@
+const Rates = {
+  CNY: 0.661,
+  PHP: 0.0818,
+  INR: 0.058179,
+  JPY: 0.032461,
+  KRW: 0.003382,
+  SGD: 3.3886,
+  MYR: 1.0104,
+  THB: 0.1277,
+};
+
+export default Rates;
