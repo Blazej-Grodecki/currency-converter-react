c703434aec344ccdfb62b6c82d81f952ae1bff6c
Blazej Grodecki <b.grodecki@gmail.com>
2023-03-28 11:38:18 +0200

    footer updated

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -2,8 +2,8 @@ import "./style.css";
 
 const Footer = () => (
   <footer className="footer">
-    <p>CNY = 0.6610, PHP = 0.0818, INR = 5.8179 / 100, JPY = 3.2461 / 100</p>
-    <p>KRW = 0.3382 / 100, SGD = 3.3886, MYR = 1.0104, THB = 0.1277</p>
+    <p>CNY = 0.6610, PHP = 0.0818, INR = 0.058179, JPY = 0.032461</p>
+    <p>KRW = 0.003382, SGD = 3.3886, MYR = 1.0104, THB = 0.1277</p>
   </footer>
 );
 
