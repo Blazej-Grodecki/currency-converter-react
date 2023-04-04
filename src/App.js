d9fdb54fa014860a987711619f4dbdab9e42c22b
Blazej Grodecki <b.grodecki@gmail.com>
2023-04-04 22:16:45 +0200

    added Button component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Form from "./Form";
 import Footer from "./Footer";
 import Container from "./Container";
 import Rates from "./Rates";
+import Button from "./Button";
 
 function App() {
   return (
     <Container>
       <Header title="Kalkulator azjatyckich walut" />
       <Form />
+      <Button />
       <p className="form__result">
         <span></span>
       </p>
