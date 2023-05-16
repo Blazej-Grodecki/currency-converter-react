335bdef86ba704d227ac01a7f739b0617400a908
Blazej Grodecki <b.grodecki@gmail.com>
2023-05-16 22:58:08 +0200

    array rates renamed to currencies and updated

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import Header from "./Header";
 import Form from "./Form";
 import Footer from "./Footer";
 import Container from "./Container";
-import Rates from "./Rates";
+import { currencies } from "./Currencies";
 import Button from "./Button";
 
 function App() {
   return (
     <Container>
       <Header title="Kalkulator azjatyckich walut" />
-      <Form />
+      <Form currencies={currencies}/>
       <Button />
       <p className="form__result">
         <span></span>
---
+++ src/Currencies/index.js
@@ -0,0 +1,10 @@
+export const currencies = [
+  {code: "CNY", name: "yuan", rate: 0.661},
+  {code: "PHP", name: "peso", rate: 0.0818},
+  {code: "INR", name: "rupia", rate: 0.058179},
+  {code: "JPY", name: "jen", rate: 0.032461},
+  {code: "KRW", name: "won", rate: 0.003382},
+  {code: "SGD", name: "dolar", rate: 3.3886},
+  {code: "MYR", name: "ringgit", rate: 1.0104},
+  {code: "THB", name: "baht", rate: 0.1277},
+];
